a4961a154f5e97240c9ebb48a85cb9f9a7715916
Danil <koime@yandex.ru>
2022-12-24 21:21:05 +0500

    Сделал определение длины нового массива

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿int size = 5;
+int newsize = 0;
 string[] words = new string[size];
 for(int i =0;i<size;i++)
 {   
     Console.WriteLine($"Введите {i+1} элемент массива");
     words[i] = Console.ReadLine();
+}
+for(int i = 0;i<size;i++)
+{
+    if(words[i].Length<=3)
+    {
+        newsize++;
+    }
 }
