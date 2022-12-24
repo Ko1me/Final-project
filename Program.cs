c390c5a6bfe0464aa2cd606c8c581ba9fe69efcd
Danil <koime@yandex.ru>
2022-12-24 16:43:02 +0500

    Сделал ввод с клавиатуры

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿int size = 5;
+string[] words = new string[size];
+for(int i =0;i<size;i++)
+{   
+    Console.WriteLine($"Введите {i+1} элемент массива");
+    words[i] = Console.ReadLine();
+}
